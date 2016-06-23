cba0ee5f4fc77b463bf0b9df04458f16c9b28512
greg <triggerman@bitbucket.org>
2016-06-23 19:39:03 -0400

    initial files

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to icpdb
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject icpdb "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+		 [http-kit "2.1.12"]]
+  :main icpdb.webapp
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
